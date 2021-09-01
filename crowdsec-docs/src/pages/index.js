d77041179d836e0341b19d7cac66de50c4ee185c
AlteredCoder <AlteredCoder>
2021-09-01 17:34:15 +0200

    update image path

--- crowdsec-docs/src/pages/index.js
+++ crowdsec-docs/src/pages/index.js
@@ -11,7 +11,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <img src="/static/img/crowdsec_logo.png" width="35%" height="35%" />
+        <img src="/img/crowdsec_logo.png" width="35%" height="35%" />
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
