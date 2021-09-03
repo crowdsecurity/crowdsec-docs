c339921e662ab85b4832097837c160a8b4d94da6
AlteredCoder <AlteredCoder>
2021-09-03 11:29:41 +0200

    add logos

---
+++ crowdsec-docs/src/components/RemediationFeatures.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './RemediationFeatures.module.css';
+
+
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+export default function RemediationsFeatures() {
+    return (
+      <section className={styles.remediations}>
+        <div className="container">
+        <h1 className="remediationTitle"> Examples of detected behaviors </h1>
+          <div style={{"justify-content": "center"}} className="row">
+            <img src={useBaseUrl('/img/remediations.png')} width="75%" height="75%" />
+          </div>
+        </div>
+      </section>
+    );
+  }
+  
