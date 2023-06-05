35c77336f4872231e39e30eedb3b0be5326916c2
alteredCoder <kevin@crowdsec.net>
2023-06-05 15:05:11 +0200

    fix errors

--- crowdsec-docs/src/components/githubIconRender.js
+++ crowdsec-docs/src/components/githubIconRender.js
@@ -2,22 +2,38 @@ import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { useEffect } from "react";
 
 
 class GithubIconRender extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { color: "black" };
+    }
+
+    componentDidMount() {
+        if (localStorage.getItem("theme") == "dark") {
+            this.setState({color: "white"});
+        } else {
+            this.setState({color: "black"});
+        }    
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.color != this.state.color) {
+            if (localStorage.getItem("theme") == "dark") {
+                this.setState({color: "white"});
+            } else {
+                this.setState({color: "black"});
+            }
+        }
     }
 
     render() {
-        var color = "black";
-        if (localStorage.getItem("theme") == "dark") {
-            color = "white";
-        }
         return (
-            <div style={{display: 'flex'}}>
-                <div style={{textAlign: 'center', flex: '1'}}>
-                    <a href={this.props.url} target="_blank"><FontAwesomeIcon icon={faGithub} size="2x" color={color}/>  </a>
+            <div style={{ display: 'flex' }}>
+                <div style={{ textAlign: 'center', flex: '1' }}>
+                    <a href={this.props.url} target="_blank"><FontAwesomeIcon icon={faGithub} size="2x" color={this.state.color} />  </a>
                     <p>Github</p>
                 </div>
             </div>
