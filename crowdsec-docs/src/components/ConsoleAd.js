c13bb2945c9314a7d102576cd60c893a78f77d41
Laurence <laurence.jones@live.co.uk>
2023-11-06 11:52:49 +0000

    Remove usememo as it was causing refresh issues

--- crowdsec-docs/src/components/ConsoleAd.js
+++ crowdsec-docs/src/components/ConsoleAd.js
@@ -1,17 +1,11 @@
-import React, {
-    useMemo,
-  } from 'react';
 import { useColorMode } from '@docusaurus/theme-common';
 
 const SIGNUP_LINK = `https://app.crowdsec.net/signup${process.env.NODE_ENV === 'production' ? '?mtm_campaign=Console&mtm_source=docs&mtm_medium=tocAd&mtm_content=Console' : ''}`
 export default function ConsoleAd() {
-    const { colorMode  } = useColorMode();
-    const image = useMemo(() => {
-        return colorMode === 'light' ? "/img/console_ad_light_alt.svg" : "/img/console_ad_dark.svg"
-      }, [colorMode]);
+    const { isDarkTheme  } = useColorMode();
     return (
         <a href={SIGNUP_LINK} target="_blank">
-            <img src={image} alt="CrowdSec Console" width="100%" />
+            <img src={isDarkTheme ? "/img/console_ad_dark.svg" : "/img/console_ad_light_alt.svg"} alt="CrowdSec Console" width="100%" />
         </a>
     );
   }
