0c2e5c84d327f4034659ae47eb5bfbe2b4d2ee93
Laurence <laurence.jones@live.co.uk>
2023-11-06 12:19:30 +0000

    Otherway is deprecated

--- crowdsec-docs/src/components/ConsoleAd.js
+++ crowdsec-docs/src/components/ConsoleAd.js
@@ -3,10 +3,10 @@ import { useColorMode } from '@docusaurus/theme-common';
 
 const SIGNUP_LINK = `https://app.crowdsec.net/signup${process.env.NODE_ENV === 'production' ? '?mtm_campaign=Console&mtm_source=docs&mtm_medium=tocAd&mtm_content=Console' : ''}`
 export default function ConsoleAd() {
-    const { isDarkTheme  } = useColorMode();
+    const { colorMode  } = useColorMode();
     return (
         <a href={SIGNUP_LINK} target="_blank">
-            <img src={isDarkTheme ? "/img/console_ad_dark.svg" : "/img/console_ad_light_alt.svg"} alt="CrowdSec Console" width="100%" />
+            <img src={colorMode === "dark" ? "/img/console_ad_dark.svg" : "/img/console_ad_light_alt.svg"} alt="CrowdSec Console" width="100%" />
         </a>
     );
   }
