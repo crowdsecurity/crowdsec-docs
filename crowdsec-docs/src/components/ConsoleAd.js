aadab02a1c051d194333f45c1f9cd7f375a72106
Laurence <laurence.jones@live.co.uk>
2023-11-06 11:17:06 +0000

    Fix how slow the ad changes between themes

--- crowdsec-docs/src/components/ConsoleAd.js
+++ crowdsec-docs/src/components/ConsoleAd.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, {
+    useMemo,
+  } from 'react';
 import { useColorMode } from '@docusaurus/theme-common';
 
 const SIGNUP_LINK = `https://app.crowdsec.net/signup${process.env.NODE_ENV === 'production' ? '?mtm_campaign=Console&mtm_source=docs&mtm_medium=tocAd&mtm_content=Console' : ''}`
 export default function ConsoleAd() {
     const { isDarkTheme } = useColorMode();
+    const image = useMemo(() => {
+        return isDarkTheme ? "/img/console_ad_dark.svg" : "/img/console_ad_light_alt.svg"
+      }, [isDarkTheme]);
     return (
         <a href={SIGNUP_LINK} target="_blank">
-            <img src={isDarkTheme ? "/img/console_ad_dark.svg" : "/img/console_ad_light_alt.svg"} alt="CrowdSec Console" width="100%" />
+            <img src={image} alt="CrowdSec Console" width="100%" />
         </a>
     );
   }
