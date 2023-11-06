4354489d0d3c6d20e429d5bc4deaab1d3a008858
Laurence Jones <laurence.jones@live.co.uk>
2023-11-06 12:46:42 +0000

    Fix on reload (#491)

--- crowdsec-docs/src/components/ConsoleAd.js
+++ crowdsec-docs/src/components/ConsoleAd.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import { useColorMode } from '@docusaurus/theme-common';
+import React from "react";
+import ThemedImage from "@theme/ThemedImage";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
-const SIGNUP_LINK = `https://app.crowdsec.net/signup${process.env.NODE_ENV === 'production' ? '?mtm_campaign=Console&mtm_source=docs&mtm_medium=tocAd&mtm_content=Console' : ''}`
+const SIGNUP_LINK = `https://app.crowdsec.net/signup${
+    process.env.NODE_ENV === "production"
+        ? "?mtm_campaign=Console&mtm_source=docs&mtm_medium=tocAd&mtm_content=Console"
+        : ""
+}`;
 export default function ConsoleAd() {
-    const { colorMode  } = useColorMode();
     return (
         <a href={SIGNUP_LINK} target="_blank">
-            <img src={colorMode === "dark" ? "/img/console_ad_dark.svg" : "/img/console_ad_light_alt.svg"} alt="CrowdSec Console" width="100%" />
+            <ThemedImage
+                alt="CrowdSec Console"
+                sources={{
+                    light: useBaseUrl("/img/console_ad_light_alt.svg"),
+                    dark: useBaseUrl("/img/console_ad_dark.svg"),
+                }}
+                width="100%"
+            />
         </a>
     );
-  }
+}
