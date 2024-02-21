f79d062cddfa52da5a7320677098a818ba7a39a4
Laurence <laurence.jones@live.co.uk>
2024-02-21 16:35:58 +0000

    wip: console wip and upgrade unixwindwstabs to support single windows command

--- crowdsec-docs/src/components/UnixWindowsTabs.js
+++ crowdsec-docs/src/components/UnixWindowsTabs.js
@@ -3,13 +3,13 @@ import TabItem from "@theme/TabItem";
 import CodeBlock from "@theme/CodeBlock";
 
 /*
-    * This component is used to display code snippets for both Unix and Windows operating systems.
-    * It uses the Docusaurus Tabs component to display the code snippets for both operating systems.
-    * The component takes in three props: bash, powershell, and cmd.
-    * The bash prop is used to display the code snippet for Unix operating systems.
-    * The powershell prop is used to display the code snippet for Windows Powershell.
-    * The cmd prop is used to display the code snippet for Windows Command Prompt.
-*/
+ * This component is used to display code snippets for both Unix and Windows operating systems.
+ * It uses the Docusaurus Tabs component to display the code snippets for both operating systems.
+ * The component takes in three props: bash, powershell, and cmd.
+ * The bash prop is used to display the code snippet for Unix operating systems.
+ * The powershell prop is used to display the code snippet for Windows Powershell.
+ * The cmd prop is used to display the code snippet for Windows Command Prompt.
+ */
 export default function UnixWindowsTabs({ ...props }) {
     return (
         <Tabs
@@ -24,25 +24,35 @@ export default function UnixWindowsTabs({ ...props }) {
                 <CodeBlock className="language-bash">{props.bash}</CodeBlock>
             </TabItem>
             <TabItem value="windows">
-                <Tabs
-                    defaultValue="powershell"
-                    groupId="windows-terminal"
-                    values={[
-                        { label: "Powershell", value: "powershell" },
-                        { label: "CMD", value: "terminal" },
-                    ]}
-                >
-                    <TabItem value="powershell">
-                        <CodeBlock className="language-bash">
-                            {props.powershell}
-                        </CodeBlock>
-                    </TabItem>
-                    <TabItem value="terminal">
-                        <CodeBlock className="language-bash">
-                            {props.cmd}
-                        </CodeBlock>
-                    </TabItem>
-                </Tabs>
+                {props.cmd === undefined && props.powershell === undefined ? (
+                    <p>Code snippet not available for Windows</p>
+                ) : null}
+                {props.cmd === undefined && props.powershell !== undefined ? (
+                    <CodeBlock className="language-bash">
+                        {props.powershell}
+                    </CodeBlock>
+                ) : null}
+                {props.cmd !== undefined && props.powershell !== undefined ? (
+                    <Tabs
+                        defaultValue="powershell"
+                        groupId="windows-terminal"
+                        values={[
+                            { label: "Powershell", value: "powershell" },
+                            { label: "CMD", value: "terminal" },
+                        ]}
+                    >
+                        <TabItem value="powershell">
+                            <CodeBlock className="language-bash">
+                                {props.powershell}
+                            </CodeBlock>
+                        </TabItem>
+                        <TabItem value="terminal">
+                            <CodeBlock className="language-bash">
+                                {props.cmd}
+                            </CodeBlock>
+                        </TabItem>
+                    </Tabs>
+                ) : null}
             </TabItem>
         </Tabs>
     );
