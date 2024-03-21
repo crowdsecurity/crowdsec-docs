d0e00e2b78cf02650515ba47ca8b3c86ba8df163
Laurence <laurence.jones@live.co.uk>
2024-03-21 14:27:35 +0000

    wip: reduce footprint of github render component

--- crowdsec-docs/src/components/githubIconRender.js
+++ crowdsec-docs/src/components/githubIconRender.js
@@ -1,23 +1,15 @@
-import React from 'react';
 import { cibGithub } from '@coreui/icons';
 import CIcon from '@coreui/icons-react';
 
-class GithubIconRender extends React.Component {
-
-    render() {
+export default function GithubIconRender({ ...props }) {
         return (
             <div style={{ display: 'flex' }}>
                 <div style={{ textAlign: 'center', flex: '1' }}>
-                    <a href={this.props.url} target="_blank" className="dark:tw-text-white tw-text-black">
+                    <a href={props.url} target="_blank" className="dark:tw-text-white tw-text-black">
                         <CIcon icon={cibGithub} size="xxl" />
                     </a>
                     <p>Github</p>
                 </div>
             </div>
         )
-    }
 }
-
-export default GithubIconRender;
-
-
