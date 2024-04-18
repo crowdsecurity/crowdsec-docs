4e1c456cecd0fb0980dd6acc2b5a739f12d68ed2
Laurence <laurence.jones@live.co.uk>
2024-04-18 14:39:29 +0100

    wip: make header text on promo smaller for smaller devices

--- crowdsec-docs/src/components/ConsolePromo.js
+++ crowdsec-docs/src/components/ConsolePromo.js
@@ -6,7 +6,7 @@ export default function ConsolePromo({ ...props }) {
         <div className="tw-flex-row tw-flex tw-bg-primary-dark tw-p-4 tw-rounded-xl tw-items-center">
             <div className="tw-flex-col tw-flex tw-py-1 tw-pr-4 tw-text-white tw-flex-1 tw-h-full">
                 {!!props.title ? (
-                    <h3 className="tw-text-3xl">{props.title}</h3>
+                    <h3 className="tw-text-center tw-text-xl sm:tw-text-2xl lg:tw-text-left lg:tw-text-3xl">{props.title}</h3>
                 ) : null}
                 {!!props.description ? <p className="tw-px-4">{props.description}</p> : null}
                 <Link
