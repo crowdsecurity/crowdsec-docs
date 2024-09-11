b40a5d45ed09bb8adae665d7197fb3ae3fceb4c8
jdv <julien@crowdsec.net>
2024-09-11 11:29:14 +0200

    adding sophos fw integration doc

--- crowdsec-docs/sidebarsUnversioned.js
+++ crowdsec-docs/sidebarsUnversioned.js
@@ -329,6 +329,7 @@ module.exports = {
                 "integrations/f5",
                 "integrations/fortinet",
                 "integrations/paloalto",
+                "integrations/sophos",
                 "integrations/genericfirewall",
                 "integrations/remediationcomponent",
             ]
