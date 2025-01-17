9cb8f90e47fcb3dc0c34f1b41a2a93206f535fcf
Laurence <laurence.jones@live.co.uk>
2025-01-17 20:03:32 +0000

    enhance: make table less jumpy on load

--- crowdsec-docs/src/components/tableRender.js
+++ crowdsec-docs/src/components/tableRender.js
@@ -1,78 +1,75 @@
-import { useState, useEffect, useMemo } from 'react';
-import { MaterialReactTable } from 'material-react-table';
-import { createTheme, ThemeProvider } from '@mui/material';
-import BrowserOnly from '@docusaurus/BrowserOnly';
-import {useColorMode} from '@docusaurus/theme-common';
+import { useState, useEffect, useMemo } from "react"
+import { MaterialReactTable } from "material-react-table"
+import { createTheme, ThemeProvider } from "@mui/material"
+import BrowserOnly from "@docusaurus/BrowserOnly"
+import { useColorMode } from "@docusaurus/theme-common"
 
-
-const TableRender = ({ columns, url, include=[], exclude=[] }) => {
+const TableRender = ({ columns, url, include = [], exclude = [] }) => {
     const [jsonContent, setJsonContent] = useState([])
-    const {colorMode} = useColorMode();
+    const [isLoading, setIsLoading] = useState(true)
+    const { colorMode } = useColorMode()
 
     const theme = useMemo(() => {
-      const isLight = colorMode === 'light';
+        const isLight = colorMode === "light"
 
-      return createTheme({
-          palette: {
-            mode: colorMode,
-            background: {
-              default:
-                isLight
-                  ? 'rgb(255,255,255)'
-                  : '#1b1b1d',
+        return createTheme({
+            palette: {
+                mode: colorMode,
+                background: {
+                    default: isLight ? "rgb(255,255,255)" : "#1b1b1d",
+                },
             },
-          },
-        },
-      );
-    }, [colorMode]);
+        })
+    }, [colorMode])
 
-
-  useEffect(() => {
+    useEffect(() => {
         fetch(url)
             .then((res) => res.json())
             .then((data) => {
-                const updatedData = [];
-                const names = [];
-
-                Object.keys(data).map((key, i) => {
+                const updatedData = []
+                const names = new Set()
+                for (const key of Object.keys(data)) {
                     // filter duplicate names
-                    const item = data[key];
-                    const name = item["name"];
-                    for (let i = 0; i < exclude.length; i++) {
-                        if (name.includes(exclude[i])) {
-                            return
-                        }
-                    }
-                    for (let i = 0; i < include.length; i++) {
-                        if (!name.includes(include[i])) {
-                            return
-                        }
-                    }
-                    if (names.includes(name)) {
-                        return
+                    const item = data[key]
+                    const name = item["name"]
+
+                    if (exclude.some((excluded) => name.includes(excluded))) {
+                        continue
                     }
 
-                    names.push(name)
+                    if (!include.every((included) => name.includes(included))) {
+                        continue
+                    }
+
+                    if (names.has(name)) {
+                        continue
+                    }
+
+                    names.add(name)
                     updatedData.push({
-                      ...item,
-                      // flattening list of strings into CSV strings allow global filtering on them
-                      // /!\ it requires special handling in the rendering side (see crowdsec-docs/docs/cti_api/taxonomy) /!\
-                      ...item.behaviors ? { behaviors: item.behaviors.join('\n') } : {},
-                      ...item.mitre_attacks ? { mitre_attacks: item.mitre_attacks.join('\n') } : {},
-                      ...item.cves ? { cves: item.cves.join('\n') } : {},
-                    });
-                });
-
-                setJsonContent(updatedData);
+                        ...item,
+                        // flattening list of strings into CSV strings to allow global filtering on them
+                        // /!\ requires special handling in the rendering side (see crowdsec-docs/docs/cti_api/taxonomy) /!\
+                        ...(item.behaviors
+                            ? { behaviors: item.behaviors.join("\n") }
+                            : {}),
+                        ...(item.mitre_attacks
+                            ? { mitre_attacks: item.mitre_attacks.join("\n") }
+                            : {}),
+                        ...(item.cves ? { cves: item.cves.join("\n") } : {}),
+                    })
+                }
+                setJsonContent(updatedData)
+                setIsLoading(false)
             })
-    // execute this fetch only once (on mount)
-    }, []);
+        // execute this fetch only once (on mount)
+    }, [])
     if (!columns || !jsonContent) {
         return <></>
     }
     return (
         <BrowserOnly>
-            {() =>
+            {() => (
                 <ThemeProvider theme={theme}>
                     <MaterialReactTable
                         data={jsonContent}
@@ -84,14 +81,14 @@ const TableRender = ({ columns, url, include=[], exclude=[] }) => {
                             rowsPerPageOptions: [10, 15, 25, 50, 100],
                         }}
                         getRowId={(row) => row.name}
+                        state={{
+                            isLoading: isLoading,
+                        }}
                     />
                 </ThemeProvider>
-            }
+            )}
         </BrowserOnly>
-    );
-
+    )
 }
 
-export default TableRender;
-
-
+export default TableRender
