2c2b7e0c7561d091ca7234d7b48aa988564b0478
Laurence Jones <laurence.jones@live.co.uk>
2025-06-19 16:07:08 +0100

    chore: docusarus v3.8 (#800)
    * chore: docusarus v3.8
    
    * chore: fixes
    
    * chore: turn off some v4 opts due to problems

--- crowdsec-docs/docusaurus.config.js
+++ crowdsec-docs/docusaurus.config.js
@@ -47,6 +47,9 @@ const backportRedirect = (s) => {
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
     future: {
+        v4: {
+            removeLegacyPostBuildHeadAttribute: true,
+        },
         experimental_faster: true,
     },
     title: "CrowdSec",
