0f3f4a795bd6ffe3ced63228f701d340383e07ce
Roussange Alexandre <roussange.alexandre@gmail.com>
2025-07-08 10:22:01 +0200

    refactor :recycle: rebase on master

--- crowdsec-docs/src/ui/hybridtooltip.tsx
+++ crowdsec-docs/src/ui/hybridtooltip.tsx
@@ -1,52 +1,49 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { PropsWithChildren, createContext, useContext, useEffect, useState } from 'react';
-import { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from './tooltip';
-import { Popover, PopoverTrigger, PopoverContent } from './popover';
-import { TooltipContentProps, TooltipProps, TooltipTriggerProps } from '@radix-ui/react-tooltip';
-import { PopoverContentProps, PopoverProps, PopoverTriggerProps } from '@radix-ui/react-popover';
+import { PopoverContentProps, PopoverProps, PopoverTriggerProps } from "@radix-ui/react-popover";
+import { TooltipContentProps, TooltipProps, TooltipTriggerProps } from "@radix-ui/react-tooltip";
+import React, { createContext, PropsWithChildren, useContext, useEffect, useState } from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "./popover";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./tooltip";
 
 const TouchContext = createContext<boolean | undefined>(undefined);
 const useTouch = () => useContext(TouchContext);
 
-export const TouchProvider = (props: PropsWithChildren) => {
-  const [isTouch, setTouch] = useState<boolean>();
+export const TouchProvider = (props: PropsWithChildren): React.JSX.Element => {
+	const [isTouch, setTouch] = useState<boolean>();
 
-  useEffect(() => {
-    setTouch(window.matchMedia('(pointer: coarse)').matches);
-  }, []);
+	useEffect(() => {
+		setTouch(window.matchMedia("(pointer: coarse)").matches);
+	}, []);
 
-  // Conditionally provide the appropriate provider
-  return (
-    <TouchContext.Provider value={isTouch}>
-      {isTouch ? (
-        // Popover doesn't need a provider
-        <>{props.children}</>
-      ) : (
-        // Tooltip needs TooltipProvider
-        <TooltipProvider delayDuration={300}>
-          {props.children}
-        </TooltipProvider>
-      )}
-    </TouchContext.Provider>
-  );
+	// Conditionally provide the appropriate provider
+	return (
+		<TouchContext.Provider value={isTouch}>
+			{isTouch ? (
+				// Popover doesn't need a provider
+				props.children
+			) : (
+				// Tooltip needs TooltipProvider
+				<TooltipProvider delayDuration={300}>{props.children}</TooltipProvider>
+			)}
+		</TouchContext.Provider>
+	);
 };
 
 export const HybridTooltip = (props: TooltipProps & PopoverProps) => {
-  const isTouch = useTouch();
+	const isTouch = useTouch();
 
-  return isTouch ? <Popover {...props} /> : <Tooltip {...props} />;
+	return isTouch ? <Popover {...props} /> : <Tooltip {...props} />;
 };
 
 export const HybridTooltipTrigger = (props: TooltipTriggerProps & PopoverTriggerProps) => {
-  const isTouch = useTouch();
+	const isTouch = useTouch();
 
-  return isTouch ? <PopoverTrigger {...props} /> : <TooltipTrigger {...props} />;
+	return isTouch ? <PopoverTrigger {...props} /> : <TooltipTrigger {...props} />;
 };
 
 export const HybridTooltipContent = (props: TooltipContentProps & PopoverContentProps) => {
-  const isTouch = useTouch();
+	const isTouch = useTouch();
 
-  return isTouch ? <PopoverContent {...props} /> : <TooltipContent {...props} />;
+	return isTouch ? <PopoverContent {...props} /> : <TooltipContent {...props} />;
 };
