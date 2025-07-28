fad2292fbdd3e3c0c42ceb29931f965fec06ec34
Laurence <laurence.jones@live.co.uk>
2025-07-28 14:55:57 +0100

    enhance: Fix styling on console promo

--- crowdsec-docs/src/components/console-promo.tsx
+++ crowdsec-docs/src/components/console-promo.tsx
@@ -6,21 +6,21 @@ import React from "react";
 const ConsolePromo = ({ ...props }): React.JSX.Element => {
 	const url = useBaseUrl(`/img/${props.image}`);
 	return (
-		<Card>
-			<CardHeader>{props.title ? <h3 className="text-left">{props.title}</h3> : null}</CardHeader>
-			<CardContent className="flex-row flex space-x-4">
-				<div className="flex-col flex text-card-foreground flex-1 h-full text-base">
-					{props.description ? <p className="md:px-4">{props.description}</p> : null}
-					<div className="text-center">
+		<Card className="min-h-[200px] flex flex-col">
+			<CardHeader className="flex-shrink-0">{props.title ? <h3 className="text-left">{props.title}</h3> : null}</CardHeader>
+			<CardContent className="flex-row flex space-x-4 flex-1">
+				<div className="flex-col flex justify-between flex-1">
+					{props.description ? <p className="text-left text-base text-card-foreground">{props.description}</p> : null}
+					<div className="text-left">
 						<a href={props.link} target="_blank">
-							<Button color="primary" className=" w-full md:w-auto">
+							<Button color="primary" className="w-full md:w-1/2">
 								{props.text ?? "Get Started"}
 							</Button>
 						</a>
 					</div>
 				</div>
 
-				{props.image ? <img src={url} alt={props.title} className="h-1/2 w-1/2 hidden md:flex" /> : null}
+				{props.image ? <img src={url} alt={props.title} className="h-1/2 w-1/2 object-contain rounded hidden md:flex" /> : null}
 			</CardContent>
 		</Card>
 	);
