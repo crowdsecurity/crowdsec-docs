ee1bcc1f44bff5b7bfb184e172c989f27a8565d4
sabban <github@sabban.eu>
2025-12-02 13:47:05 +0100

    format

--- crowdsec-docs/src/components/home-page/quick-start.tsx
+++ crowdsec-docs/src/components/home-page/quick-start.tsx
@@ -110,11 +110,11 @@ const QuickStart = (): React.JSX.Element => {
 			</div>
 			<div className="grid grid-cols-1 gap-4 mt-4">
 				<HomePageItem
-                    title="Installation Heathcheck"
-                    description=""
-                    link="/u/getting_started/health_check"
-                    icon={MonitorHeartIcon}
-                    key="Installation Heathcheck"
+                  title="Installation Heathcheck"
+                  description=""
+                  link="/u/getting_started/health_check"
+                  icon={MonitorHeartIcon}
+                  key="Installation Heathcheck"
                 />
 			</div>
 			<p className="text-xs mt-1 mb-4 text-foreground/70 text-right">
