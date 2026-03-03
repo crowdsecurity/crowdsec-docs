db8c3fd6a07559f622fcedd2164972aacefe2a18
Lucas Cherifi <lucas.cherifi@gmail.com>
2026-03-03 13:06:21 +0100

    feat(leadfeeder): implement tracking functionality with new tracker module

--- crowdsec-docs/plugins/leadfeeder/index.js
+++ crowdsec-docs/plugins/leadfeeder/index.js
@@ -1,29 +1,12 @@
+var path = require("path")
+
 module.exports = function () {
     const isProd = process.env.NODE_ENV === "production"
 
     return {
         name: "docusaurus-plugin-leadfeeder",
-        injectHtmlTags() {
-            if (!isProd) {
-                return {}
-            }
-            return {
-                headTags: [
-                    {
-                        tagName: "script",
-                        innerHTML: `
-              window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));};
-              (function(d,s){
-                var fs=d.getElementsByTagName(s)[0];
-                var cs=d.createElement(s);
-                cs.src='https://sc.lfeeder.com/lftracker_v1_lAxoEaKqxpv7OYGd.js';
-                cs.async=1;
-                fs.parentNode.insertBefore(cs,fs);
-              })(document,'script');
-            `,
-                    },
-                ],
-            }
+        getClientModules() {
+            return isProd ? [path.resolve(__dirname, "./tracker")] : []
         },
     }
 }
---
+++ crowdsec-docs/plugins/leadfeeder/tracker.js
@@ -0,0 +1,33 @@
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment"
+
+let loaded = false
+
+function loadLeadfeeder() {
+    if (loaded) return
+    loaded = true
+
+    window.ldfdr =
+        window.ldfdr ||
+        function () {
+            ;(ldfdr._q = ldfdr._q || []).push([].slice.call(arguments))
+        }
+
+    var fs = document.getElementsByTagName("script")[0]
+    var cs = document.createElement("script")
+    cs.src = "https://sc.lfeeder.com/lftracker_v1_lAxoEaKqxpv7OYGd.js"
+    cs.async = 1
+    fs.parentNode.insertBefore(cs, fs)
+}
+
+const clientModule = {
+    onRouteDidUpdate() {
+        if (!ExecutionEnvironment.canUseDOM) return
+
+        const consent = JSON.parse(localStorage.getItem("docusaurus.cookieConsent") ?? "null")
+        if (!consent) return
+
+        loadLeadfeeder()
+    },
+}
+
+export default clientModule
